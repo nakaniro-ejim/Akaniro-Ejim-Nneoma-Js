a51b4ea17aa7565420736bad5efe11affabe17dc
Nneoma <>
2022-07-15 10:38:12 +0000

    check challenge 1 check challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0;
+  for (let num of arrayOfNumbers){
+    sum += num;
+  }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let totalEvenNum = 0;
+  for (let num of arrayOfNumbers){
+    if (num % 2 === 0){
+      totalEvenNum += 1;
+    }
+  }
+  
+    return totalEvenNum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
